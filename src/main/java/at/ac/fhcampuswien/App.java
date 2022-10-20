9d7f8700bdc953e4deae1036eb521a26780cd174
000Mousavi <zahra.mousavi@stud.fh-campuswien.ac.at>
2022-10-20 15:08:23 +0200

    Task 1

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 
 public class App {
 
-    // Implement all methods as public static
+    // Task 1: OneMonthCalendar
+    public static void oneMonthCalendar(int days, int firstDay) {
+        int weekCounter = firstDay - 1;
 
-    public static void main(String[] args) {
-        // test your method implementations here
-        // make method calls
-        // print their results
-        // etc.
-    }
-}
+        for (int j = 1; j <= 3 * (firstDay - 1); j++) {
+            System.out.print(" ");
+        }
+        for (int i = 1; i <= days; i++) {
+            System.out.printf("%2d", i);
+            System.out.print(" ");
+            weekCounter++;
+            if (weekCounter == 7) {
+                System.out.println();
+                weekCounter = 0;
+            }
+        }
+        if (weekCounter != 0) System.out.println();
+    }}
+
+
